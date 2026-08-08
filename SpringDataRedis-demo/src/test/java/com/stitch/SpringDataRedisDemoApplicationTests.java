dba3e0f2260b42b5f551625bfffc06d06ae42ca9
tw <17357596+twabc@user.noreply.gitee.com>
2026-08-08 16:02:03 +0800

    SpringDataRedis快速入门

---
+++ SpringDataRedis-demo/src/test/java/com/stitch/SpringDataRedisDemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.stitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+@SpringBootTest
+class SpringDataRedisDemoApplicationTests {
+
+	@Autowired
+	private RedisTemplate redisTemplate;
+	@Test
+	void RedisTest() {
+		ValueOperations ops = redisTemplate.opsForValue();
+		ops.set("name", "stitch2");
+		System.out.println(ops.get("name"));
+	}
+
+}

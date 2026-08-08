249e7a60bd762e9ce5b783e7d10f0a938bad92d9
tw <17357596+twabc@user.noreply.gitee.com>
2026-08-08 14:45:59 +0800

    jedis快速入门

---
+++ jedis-Demo/src/test/java/com/stitch/JedisTest.java
@@ -0,0 +1,44 @@
+package com.stitch;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import redis.clients.jedis.Jedis;
+
+import java.util.Map;
+
+public class JedisTest {
+
+    private Jedis jedis;
+
+    @BeforeEach
+    public void setUp() {
+        jedis = new Jedis("localhost", 6379);
+        jedis.auth("123456");
+        jedis.select(2);
+    }
+
+    @Test
+    public void test() {
+        String result = jedis.set("name", "stitch");
+        System.out.println("result = " + result);
+        String name = jedis.get("name");
+        System.out.println(name);
+    }
+
+    @Test
+    public void test2() {
+        jedis.hset("user:4", "name", "zhangsan");
+        jedis.hset("user:4", "age", "19");
+        Map<String, String> all = jedis.hgetAll("user:4");
+        System.out.println(all);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (jedis != null) {
+            jedis.close();
+        }
+    }
+}
